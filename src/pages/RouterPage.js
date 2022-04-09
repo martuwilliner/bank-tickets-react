e9f119242fff853ef35cd35e76bcae9d46465a86
martuwilliner <williner.martin@gmail.com>
2022-04-09 01:48:31 -0300

    Router v6 listo

--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -7,22 +7,36 @@ import {
   UploadOutlined,
 } from '@ant-design/icons';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import {Ingresar} from './Ingresar';
+import { Cola } from './Cola';
+import { CrearTicket } from './CrearTicket';
+import { Escritorio } from './Escritorio';
+ 
 const { Sider, Content } = Layout;
 
 export const RouterPage = () => {
   return (
+    <Router>
     <Layout style={{height: '100vh'}}>
     <Sider>
       <div className="logo" />
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
         <Menu.Item key="1" icon={<UserOutlined />}>
-          Ingresar
+          <Link to="/ingresar">Ingresar</Link>
         </Menu.Item>
         <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-          Cola
+        
+        <Link to="/cola">Cola de tickets</Link>
         </Menu.Item>
         <Menu.Item key="3" icon={<UploadOutlined />}>
-          Crear Ticket
+          <Link to="/crear">Crear Ticket</Link>
         </Menu.Item>
       </Menu>
     </Sider>
@@ -35,9 +49,17 @@ export const RouterPage = () => {
           minHeight: 280,
         }}
       >
-        Content
+        <Routes>
+          <Route path="/ingresar" element={<Ingresar />} />
+          <Route path="/cola" element={<Cola />} />
+          <Route path="/crear" element={<CrearTicket />} />
+          <Route path="/escritorio" element={<Escritorio />} />
+
+          <Route path="*" element={<Ingresar/>} />
+        </Routes>
       </Content>
     </Layout>
   </Layout>
+  </Router>
     )
 }
